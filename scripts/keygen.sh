c4a4a9f33ca5a55e92ed44043f1f5c2e6c0a9515
Volodymyr <vovanok1992@gmail.com>
2020-05-08 23:11:48 +0300

    added test SSO k8s config

--- scripts/keygen.sh
+++ scripts/keygen.sh
@@ -26,17 +26,20 @@ openssl x509 -outform pem -in out/RootCA.pem -out out/RootCA.crt
 openssl req -new -nodes -newkey rsa:2048 -keyout out/localhost.key -out out/localhost.csr -subj "/C=US/ST=YourState/L=YourCity/O=Example-Certificates/CN=$domain"
 openssl x509 -req -sha256 -days 1024 -in out/localhost.csr -CA out/RootCA.pem -CAkey out/RootCA.key -CAcreateserial -extfile out/domains.ext -out out/localhost.crt
 
+# Add cetrs to the linux local registry
 sudo cp out/RootCA.pem /usr/local/share/ca-certificates/RootCA.crt
 sudo update-ca-certificates
 
 certfile="out/RootCA.pem"
 certname="My Root CA"
 
-
+# Add certs to the browsers DBs registries
 for certDB in $(find ~/ -name "cert9.db")
 do
     certdir=$(dirname ${certDB});
     certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d sql:${certdir}
 done
 
+# Clear and add kubernetes TLS secret for the localhost 
+kubectl delete secret tls localhost-cert 
 kubectl create secret tls localhost-cert --key out/localhost.key --cert out/localhost.crt
